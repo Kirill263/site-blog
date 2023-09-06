8845a34c837603c8cff3f46786ddf29d3d29fdf1
Кирилл <myakiishevkirill@gmail.com>
2023-09-06 23:35:45 +1000

    created models of category, tag, post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='Url_category', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, verbose_name='Url_tag', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='Url_post', unique=True)
+    author = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликовано')
+    photo = models.ImageField(upload_to='photos', blank=True)
+    view = models.IntegerField(default=0, verbose_name='Количество просмотров')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['created_at']
