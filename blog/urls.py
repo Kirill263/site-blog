f444a5d81fecdb3e6f3aa49303d92bd18b387876
Кирилл <myakiishevkirill@gmail.com>
2023-09-09 12:52:21 +1000

    added caching of the main page and highlighting of the active menu item

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('categroy/<str:slug>', get_category, name='category'),
+    path('categroy/<str:slug>/', get_category, name='category'),
 ]
